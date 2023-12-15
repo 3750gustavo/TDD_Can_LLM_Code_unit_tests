6c73523b1c8abdda7d2f9bce611dec1813e25f49
3750gustavo <gustavobarros0808@hotmail.com>
2023-12-15 15:25:45 -0300

    Refactor unit tests for Excel sorting

--- Unit_tests_python/EX-02-Excel_sort/Excel_Test.py
+++ Unit_tests_python/EX-02-Excel_sort/Excel_Test.py
@@ -1,25 +1,41 @@
-import pytest
+import tempfile
 import pandas as pd
-from openpyxl import load_workbook
+import os
+import pytest
+from pathlib import Path
 from ChatGPT import excel_sort as ChatGPT
 from CodeLLama import excel_sort as CodeLLama
 from Bard import excel_sort as Bard
 
-# Define a list of implementations to test
-implementations = [ChatGPT,CodeLLama,Bard]
+implementations = [ChatGPT, CodeLLama, Bard]
 
-@pytest.mark.parametrize("excel_sort", implementations)
-def test_sort_excel_data(excel_sort):
-    # Define the path to the test Excel file
-    test_file = "GPUs.xlsx"
+@pytest.fixture(params=implementations, ids=[impl.__name__ for impl in implementations])
+def implementation(request):
+    return request.param
 
-    # Sort the data
-    sorted_data = excel_sort(test_file, 0)
+@pytest.fixture
+def file_path():
+    return os.path.join(os.path.dirname(__file__), 'GPUs.xlsx')
 
-    # Check if the sorted data is in descending order for each column
-    for column in sorted_data.columns:
-        assert sorted_data[column].is_monotonic_decreasing
+def test_excel_sort(implementation, file_path):
+    column_index = 1
+    try:
+        result = implementation(file_path, column_index)
 
-# Run the tests
+        if isinstance(result, str) and Path(result).suffix == '.xlsx':
+            df_temp = pd.read_excel(result)
+        elif isinstance(result, bytes):
+            with tempfile.NamedTemporaryFile(suffix='.xlsx', delete=True) as temp:
+                temp.write(result)
+                temp.seek(0)
+                df_temp = pd.read_excel(temp.name)
+        else:
+            pytest.fail(f"Invalid return type from {implementation.__name__}")
+
+        assert df_temp.iloc[:, column_index].is_monotonic_decreasing, \
+            f"DataFrame is not sorted correctly in {implementation.__name__}"
+    except Exception as e:
+        pytest.fail(f"Test failed for {implementation.__name__}: {str(e)}")
+
 if __name__ == "__main__":
-    pytest.main([__file__])
+    pytest.main()
