76540e3e7ba20ed4b896d7d183cd0ecacd9da8ef
3750gustavo <gustavobarros0808@hotmail.com>
2024-02-09 14:46:01 -0300

    Redid ChaGPT and CodeLLama with new prompt

--- Unit_tests_python/EX-04-Calculate_stats/ChatGPT.py
+++ Unit_tests_python/EX-04-Calculate_stats/ChatGPT.py
@@ -3,22 +3,18 @@ import statistics
 def calculate_stats(list1, list2, list3):
     stats_dict = {}
     
-    # Calculate mean and standard deviation for list1
-    mean1 = statistics.mean(list1)
-    stddev1 = statistics.stdev(list1)
-    stats_dict['mean_list1'] = mean1
-    stats_dict['stddev_list1'] = stddev1
+    # Calculate mean and standard deviation for each list
+    mean_list1 = statistics.mean(list1)
+    stddev_list1 = statistics.stdev(list1)
     
-    # Calculate mean and standard deviation for list2
-    mean2 = statistics.mean(list2)
-    stddev2 = statistics.stdev(list2)
-    stats_dict['mean_list2'] = mean2
-    stats_dict['stddev_list2'] = stddev2
+    mean_list2 = statistics.mean(list2)
+    stddev_list2 = statistics.stdev(list2)
     
-    # Calculate mean and standard deviation for list3
-    mean3 = statistics.mean(list3)
-    stddev3 = statistics.stdev(list3)
-    stats_dict['mean_list3'] = mean3
-    stats_dict['stddev_list3'] = stddev3
+    mean_list3 = statistics.mean(list3)
+    stddev_list3 = statistics.stdev(list3)
+    
+    # Populate the dictionary with calculated values
+    stats_dict['mean'] = [mean_list1, mean_list2, mean_list3]
+    stats_dict['stddev'] = [stddev_list1, stddev_list2, stddev_list3]
     
     return stats_dict
--- Unit_tests_python/EX-04-Calculate_stats/CodeLLama.py
+++ Unit_tests_python/EX-04-Calculate_stats/CodeLLama.py
@@ -1,6 +1,15 @@
-import statistics
+import numpy as np
 
 def calculate_stats(list1, list2, list3):
-    means = [mean(list1), mean(list2), mean(list3)]
-    stddevs = [stddev(list1), stddev(list2), stddev(list3)]
+    """
+    Calculate the mean and standard deviation for each list of numbers.
+
+    Args:
+        list1, list2, list3: Lists of numbers.
+
+    Returns:
+        A dictionary with keys 'mean' and 'stddev' containing the mean and standard deviation for each list.
+    """
+    means = [np.mean(list1), np.mean(list2), np.mean(list3)]
+    stddevs = [np.std(list1), np.std(list2), np.std(list3)]
     return {'mean': means, 'stddev': stddevs}
