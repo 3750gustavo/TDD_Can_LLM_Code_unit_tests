42bff2199d499e8121bbaff1c15b16132ef4a290
3750gustavo <gustavobarros0808@hotmail.com>
2024-03-06 15:41:53 -0300

    Refactor folder setup and add function to generate test folders with n files

--- Unit_tests_python/EX-01-compare_directories/config.py
+++ Unit_tests_python/EX-01-compare_directories/config.py
@@ -88,12 +88,50 @@ def delete_files(dir1=dir1, dir2=dir2):
             file_path = os.path.join(directory, filename)
             os.remove(file_path)
 
-def setup_folders(dir1=dir1, dir2=dir2):
-    '''Create the folders if they do not exist and create the files inside them'''
+def check_make_dirs(dir1=dir1, dir2=dir2):
     for directory in [dir1, dir2]:
         if not os.path.exists(directory):
             os.makedirs(directory)
+
+def setup_folders(dir1=dir1, dir2=dir2):
+    '''Create the folders if they do not exist and create the files inside them'''
+    check_make_dirs()
     create_files_all_same_date()
     create_files_with_differences()
     # returns false if the folders any folder is empty at the end of the setup
     return not (not os.listdir(dir1) or not os.listdir(dir2))
+
+def reset_folders(dir1=dir1, dir2=dir2):
+    '''Delete all files from the folders'''
+    delete_files()
+
+def generate_big_o_test_folders_n():
+    '''Generate two folders with n files each with half same date and half different date'''
+    check_make_dirs()
+    def generate(n):
+        for i in range(n):
+            file_content = f'This is file {i} content'
+            modification_time = datetime.datetime(2021, 1, 1)
+            for directory in [dir1, dir2]:
+                file_path = os.path.join(directory, f'file{i}.txt')
+                with open(file_path, 'w') as file:
+                    file.write(file_content)
+                # Set modification date to 2021-01-01
+                set_date(file_path)
+        # Create two files with same content but different modification date in both folders (current date in dir2)
+        for i in range(n//2, n):
+            # same content for both files
+            file_content = f'This is file {i} content'
+            # base modification date
+            modification_time = datetime.datetime(2021, 1, 1)
+            # create file in dir1
+            file_path = os.path.join(dir1, f'file{i}.txt')
+            with open(file_path, 'w') as file:
+                file.write(file_content)
+            # Set modification date to 2021-01-01
+            set_date(file_path)
+            # create file in dir2
+            file_path = os.path.join(dir2, f'file{i}.txt')
+            with open(file_path, 'w') as file:
+                file.write(file_content)
+    return lambda n: generate(n)
